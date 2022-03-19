3fa818c1e6a11ecd56d6b3d400cd1837eb36d810
Zion Yang <zionremnant@gmail.com>
2022-03-18 21:51:07 -0700

    navbar index html

--- script.js
+++
@@ -1,4 +0,0 @@
-var mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-var menu_item = document.querySelector('.header .nav-bar .nav-list ul li a');
-var header = document.querySelector('.header.container');
-
